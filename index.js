5ca690b56cfa3d10173ecdab0d199d9ed51d2153
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-04-26 13:55:01 +0200

    Javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+let HomeButton = document.getElementsByClassName("HomeButton")[0];
+if (HomeButton != null){
+    HomeButton.style.cursor = "pointer";
+    HomeButton.addEventListener("click",function(){ location.href = "index.html" ; });
+}
