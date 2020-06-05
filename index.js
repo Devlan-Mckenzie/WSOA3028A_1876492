a939bd74316401606fcd9ac1c10b9a481a1a5e30
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-06-05 10:40:13 +0200

    Theory Button test

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ if (CharacterButton != null){
     CharacterButton.style.cursor = "pointer";
     CharacterButton.addEventListener("click",function(){ location.href = "../../../../../WSOA3028A_1876492/Character/Character.html" ; });
 }
+
+let TheoryButton = document.getElementsByClassName("TheoryButton")[0];
+if (TheoryButton != null){
+    TheoryButton.style.cursor = "pointer";
+    TheoryButton.addEventListener("click",function(){ location.href = "../../../../../WSOA3028A_1876492/Theory/Theory.html" ; });
+}
