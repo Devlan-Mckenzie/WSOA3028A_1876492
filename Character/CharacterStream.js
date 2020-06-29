8319a946ab5292bf484693b6a25a90fc80d58269
Devlan-Mckenzie <53466124+Devlan-Mckenzie@users.noreply.github.com>
2020-06-29 10:06:36 +0200

    Stream API Blog

--- Character/CharacterStream.js
+++ Character/CharacterStream.js
@@ -397,3 +397,44 @@ fetch('../Character/Images/ResearchPaper.png')
 // Update image
 .then(url => image10.src = url)
 .catch(console.error);
+
+
+
+
+const image11 = document.getElementById('Image_Stream');
+
+// Fetch the original image
+fetch('../Character/Images/ResearchPaper.png')
+// Retrieve its body as ReadableStream
+.then(response => response.body)
+.then(rs => {
+  const reader = rs.getReader();
+
+  return new ReadableStream({
+    async start(controller) {
+      while (true) {
+        const { done, value } = await reader.read();
+
+        // When no more data needs to be consumed, break the reading
+        if (done) {
+          break;
+        }
+
+        // Enqueue the next data chunk into our target stream
+        controller.enqueue(value);
+      }
+
+      // Close the stream
+      controller.close();
+      reader.releaseLock();
+    }
+  })
+})
+// Create a new response out of the stream
+.then(rs => new Response(rs))
+// Create an object URL for the response
+.then(response => response.blob())
+.then(blob => URL.createObjectURL(blob))
+// Update image
+.then(url => image11.src = url)
+.catch(console.error);
